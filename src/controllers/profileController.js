f0f4c72d1f816dc482353aac6a1fb678c1780f43
vitor-risso <vitor.risso13@gmail.com>
2021-04-10 14:18:51 -0300

    adding try catch at model/profile and validate vacations-per-year (if > 52 response status = 400)

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -6,6 +6,7 @@ module.exports = {
 
   async update(req, res) {
     const profile = await Profile.get()
+
     const data = req.body
     const weekPerYear = 52
     const weekPerMonth = (weekPerYear - data["vacation-per-year"]) / 12
@@ -14,12 +15,20 @@ module.exports = {
 
     const valueHour = data["monthly-budget"] / monthlyTotalHours
 
-    await Profile.update({
-      ...profile,
-      ...req.body,
-      "value-hour": valueHour
-    })
+    if (req.body["vacation-per-year"] > 52) {
+      return res.status(400).send({meesage: "Período de férias deve ser menor que 52 semanas"}).redirect('/profile')
+    }
 
-    return res.redirect('/profile')
+    try {
+      await Profile.update({
+        ...profile,
+        ...req.body,
+        "value-hour": valueHour
+      })
+    } catch (error) {
+      return res.status(500).send({ messgae: "Erro na hora de salvar no banco de dados. Internal server error" }).redirect('/profile')
+    }
+
+    return res.status(200).redirect('/profile')
   },
 }
