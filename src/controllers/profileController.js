faa34d0b9ba01b34e67e133735b133eebb05451e
vitor-risso <vitor.risso13@gmail.com>
2021-04-16 17:27:00 -0300

    clear some .render

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -22,11 +22,11 @@ module.exports = {
     const valueHour = profileUtils.valueHour(data)
 
     if (profileUtils.validateVacations(data["vacation-per-year"])) {
-      return res.status(400).send({ message: "Período de férias deve ser menor que 52 semanas" }).redirect('/profile')
+      return res.status(400).send({ message: "Período de férias deve ser menor que 52 semanas" })
     }
 
     if (profileUtils.validateDaysPerWeek(data["days-per-week"])) {
-      return res.status(400).send({ message: "Os dias trabalhados durante a semana devem ser menores ou igual a 7" }).redirect('/profile')
+      return res.status(400).send({ message: "Os dias trabalhados durante a semana devem ser menores ou igual a 7" })
     }
 
     try {
@@ -36,7 +36,7 @@ module.exports = {
         "value-hour": valueHour
       })
     } catch (error) {
-      return res.status(500).send({ messgae: "Erro na hora de salvar no banco de dados. Internal server error" }).redirect('/profile')
+      return res.status(500).send({ messgae: "Erro na hora de salvar no banco de dados. Internal server error" })
     }
 
     return res.status(200).redirect('/profile')
